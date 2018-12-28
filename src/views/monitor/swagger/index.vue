58d7ae4c7a4903f42770d0e6f78d4324426833e3
allenyll <806141743@qq.com>
2018-12-28 15:05:32 +0800

    no message

--- src/views/monitor/swagger/index.vue
+++ src/views/monitor/swagger/index.vue
@@ -1,38 +1,38 @@
 <template>
   <div>
-    <mu-circular-progress v-if="loading" :size="40"/>
-    <div v-html="html"/>
+    <mu-circular-progress :size="40" v-if="loading"/>
+    <div v-html="html"></div>
   </div>
 </template>
 <style>
 
 </style>
 <script>
-import request from '@/utils/request'
-export default{
-  // 使用时请使用 :url.sync=""传值
-  props: {
-    url: ''
-  },
-  data() {
-    return {
-      loading: false,
-      html: '<h1>qwqqwq</h1>'
+  import request from '@/utils/request'
+  export default{
+    // 使用时请使用 :url.sync=""传值
+    props: {
+      url: ''
+    },
+    data () {
+      return {
+        loading: false,
+        html: '<h1>qwqqwq</h1>'
+      }
+    },
+    watch: {
+      url (value) {
+        this.load(value)
+      }
+    },
+    mounted () {
+      this.html = request({
+          url: 'https://localhost:8082/swagger-ui.html',
+          method: 'get'
+      })
+    },
+    methods: {
+      
     }
-  },
-  watch: {
-    url(value) {
-      this.load(value)
-    }
-  },
-  mounted() {
-    return request({
-      url: 'https://localhost:8082/swagger-ui.html',
-      method: 'get'
-    })
-  },
-  methods: {
-
   }
-}
 </script>
